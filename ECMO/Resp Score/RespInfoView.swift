eb6e48d2f7c646487e7c3e6ca08f3ea5c665400b
Michael J Lazar, MD <mjlmobile@me.com>
2024-05-12 15:41:09 -0400

    Improved graph rotation

--- ECMO/Resp Score/RespInfoView.swift
+++ ECMO/Resp Score/RespInfoView.swift
@@ -8,24 +8,19 @@
 import SwiftUI
 
 struct RespInfoView: View {
-	var score: Score
+	private var score: Score
+    @State private var orientation = UIDeviceOrientation.portrait
 	
-	private func placeLine(width: Double) -> Double {
+	func placeLine(width: Double) -> Double {
 		let start = 31.0
 		let quantityTicks = width/21
-		var limitedScore = 0.0
-		if score.score < (-8) {
-			limitedScore = -8.5
-			print("<-8")
-		} else if score.score >= 8 {
+		var limitedScore = score.score
+		if limitedScore < -8 {
+			limitedScore = -9
+		} else if limitedScore >= 8 {
 			limitedScore = 8
-			print(">8")
-		} else {
-			limitedScore = Double(score.score)
-			print("basic")
 		}
-		let movement = limitedScore * quantityTicks
-		print("Limited Score: \(limitedScore)")
+		let movement = Double(limitedScore) * quantityTicks
 		return start + movement
 	}
 	
@@ -33,51 +28,75 @@ struct RespInfoView: View {
 		if let score {
 			self.score = score
 		} else {
-			self.score = Score(score: -11,
+			self.score = Score(score: 7,
 							   riskClass: "II",
 							   survivalPercent: "50")
 		}
 	}
 	
-	var body: some View {
-		VStack {
-			Spacer()
-			HStack {
-				Text("Resp Score \(score.score)")
-				Spacer()
-				Text("Risk Class \(score.riskClass)")
-				Spacer()
-				Text("Survival \(score.survivalPercent)%")
-			}
-			.padding(.vertical)
-			GeometryReader { proxy in
-				ZStack {
-					Color.white
-//						.opacity(0.2)
-					Rectangle()
-						.fill(.red)
-						.frame(minWidth: 5, idealWidth: 10, maxWidth: 10, minHeight: 50, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 270, alignment: .leading)
-						.offset(x: placeLine(width: proxy.size.width))
-					Image(ImageResource.respLines)
-						.resizable()
-						.scaledToFit()
-				}
-			}
-			.frame(minWidth: 0, maxWidth: 400, minHeight: 0, maxHeight: 300)
-			.padding(.bottom)
-			Link(destination: /*@START_MENU_TOKEN@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/) {
-				Text(RespScore.citationStr)
-					.font(.caption)
-					.italic()
-					.foregroundStyle(.blue)
-			}
-			Spacer()
-			Text(RespScore.purposeStr)
-				.font(.caption)
-				.navigationTitle(RespScore.scoreTitle)
-		}
-		.padding()
-	}
+    var body: some View {
+        VStack {
+            if orientation.isLandscape {
+                GeometryReader { proxy in
+                    ZStack {
+                        Color.gray
+                            .opacity(0.2)
+                        Rectangle()
+                            .fill(.red)
+                            .frame(minWidth: 5, idealWidth: 10, maxWidth: 10, minHeight: 50, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 300, alignment: .leading)
+                            .offset(x: placeLine(width: proxy.size.width))
+                        Image(ImageResource.respLines)
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: 400, minHeight: 0, maxHeight: 350)            } else {
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Text("Resp Score \(score.score)")
+                            Spacer()
+                            Text("Risk Class \(score.riskClass)")
+                            Spacer()
+                            Text("Survival \(score.survivalPercent)%")
+                        }
+                        .padding(.vertical)
+                        GeometryReader { proxy in
+                            ZStack {
+                                Color.gray
+                                    .opacity(0.2)
+                                Rectangle()
+                                    .fill(.red)
+                                    .frame(minWidth: 5, idealWidth: 10, maxWidth: 10, minHeight: 50, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 270, alignment: .leading)
+                                    .offset(x: placeLine(width: proxy.size.width))
+                                Image(ImageResource.respLines)
+                                    
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                        }
+                        .frame(minWidth: 0, maxWidth: 400, minHeight: 0, maxHeight: 300)
+                        .padding(.bottom)
+                        Link(destination: URL(string: "http://www.respscore.com")!) {
+                            Text(RespScore.citationStr)
+                                .font(.caption)
+                                .italic()
+                                .foregroundStyle(.blue)
+                        }
+                        Spacer()
+                        Text(RespScore.purposeStr)
+                            .font(.caption)
+                            .navigationTitle(SaveScore.scoreTitle)
+                    }
+                    .padding()
+                }
+        }
+        .onRotate { newOrientation in
+            if newOrientation.isValidInterfaceOrientation {
+                orientation = newOrientation
+            }
+        }
+    }
 }
 
 #Preview {
--- ECMO/Save Score/SaveInfoView.swift
+++ ECMO/Save Score/SaveInfoView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct SaveInfoView: View {
-	var score: Score
-	
+	private var score: Score
+    @State private var orientation = UIDeviceOrientation.portrait
+    
 	func placeLine(width: Double) -> Double {
 		let start = 31.5
 		let quantityTicks = width/42.8
@@ -29,43 +30,66 @@ struct SaveInfoView: View {
 	}
 	
     var body: some View {
-		VStack {
-			Spacer()
-			HStack {
-				Text("Save Score \(score.score)")
-				Spacer()
-				Text("Risk Class \(score.riskClass)")
-				Spacer()
-				Text("Survival \(score.survivalPercent)%")
-			}
-			.padding(.vertical)
-			GeometryReader { proxy in
-				ZStack {
-					Color.white
-//						.opacity(0.2)
-					Rectangle()
-						.fill(.red)
-						.frame(minWidth: 5, idealWidth: 10, maxWidth: 10, minHeight: 50, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 300, alignment: .leading)
-						.offset(x: placeLine(width: proxy.size.width))
-					Image(ImageResource.saveLines)
-						.resizable()
-						.scaledToFit()
-				}
-			}
-			.frame(minWidth: 0, maxWidth: 400, minHeight: 0, maxHeight: 350)
-			.padding(.bottom)
-			Link(destination: /*@START_MENU_TOKEN@*/URL(string: "https://www.apple.com")!/*@END_MENU_TOKEN@*/) {
-				Text(SaveScore.citationStr)
-					.font(.caption)
-					.italic()
-					.foregroundStyle(.blue)
-			}
-			Spacer()
-			Text(SaveScore.purposeStr)
-				.font(.caption)
-				.navigationTitle(SaveScore.scoreTitle)
-		}
-		.padding()
+        VStack {
+            if orientation.isLandscape {
+                GeometryReader { proxy in
+                    ZStack {
+                        Color.gray
+                            .opacity(0.2)
+                        Rectangle()
+                            .fill(.red)
+                            .frame(minWidth: 5, idealWidth: 10, maxWidth: 10, minHeight: 50, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 300, alignment: .leading)
+                            .offset(x: placeLine(width: proxy.size.width))
+                        Image(ImageResource.saveLines)
+                            .resizable()
+                            .scaledToFit()
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: 400, minHeight: 0, maxHeight: 350)            } else {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Text("Save Score \(score.score)")
+                        Spacer()
+                        Text("Risk Class \(score.riskClass)")
+                        Spacer()
+                        Text("Survival \(score.survivalPercent)%")
+                    }
+                    .padding(.vertical)
+                    GeometryReader { proxy in
+                        ZStack {
+                            Color.gray
+                                .opacity(0.2)
+                            Rectangle()
+                                .fill(.red)
+                                .frame(minWidth: 5, idealWidth: 10, maxWidth: 10, minHeight: 50, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 300, alignment: .leading)
+                                .offset(x: placeLine(width: proxy.size.width))
+                            Image(ImageResource.saveLines)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    }
+                    .frame(minWidth: 0, maxWidth: 400, minHeight: 0, maxHeight: 350)
+                    .padding(.bottom)
+                    Link(destination: URL(string: "https://www.elso.org/savescore/index.html")!) {
+                        Text(SaveScore.citationStr)
+                            .font(.caption)
+                            .italic()
+                            .foregroundStyle(.blue)
+                    }
+                    Spacer()
+                    Text(SaveScore.purposeStr)
+                        .font(.caption)
+                        .navigationTitle(SaveScore.scoreTitle)
+                }
+                .padding()
+            }
+        }
+        .onRotate { newOrientation in
+            if newOrientation.isValidInterfaceOrientation {
+                orientation = newOrientation
+            }
+        }
     }
 }
 
